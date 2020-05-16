6b0f8db1d68686a3f8f3a70bca73a0c3c60cc1e0
ylcarolcui <yiling_1212@hotmail.com>
2020-05-16 16:07:17 -0400

    added post controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,8 +4,19 @@ const router = express.Router();
 
 // POST
 router.post('/', (req, res) => {
-    Post.create(req.body)
-        .then((post) => res.json(post));
-})
+	Post.create(req.body).then((post) => res.json(post));
+});
 
+// Put
+router.put('/:id', (req, res) => {
+	Post.findOneAndUpdate({ _id: req.params.id }, req.body, {
+		new: true,
+	}).then((post) => res.json(post));
+});
+
+//Delete
+router.delete('/:id', (req, res) => {
+    Post.findOneAndDelete({ _id: req.params.id })
+    .then((post) => res.json(post));
+});
 module.exports = router;
